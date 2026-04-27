d3f1363b5ef45184712e1360d3b2e29a63d8c513
pgm-thijsdewaegeneer <thijdewa@student.arteveldehs.be>
2026-04-27 23:15:37 +0200

    Made questions a global variable. Added event listener to possible answers. Added function to check answers and keep score.

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // Importeer beide functies uit de controller om toegang te krijgen tot de vragen in het JSON bestand (onze database)
 import { getEasyQuestions, getMediumQuestions } from "./controllers/questions.controller.js";
 
+let currentQuestionIndex = 0;
+let questions = [];
+let score = 0;
+
 // Geef de vraag weer in de browser
 function renderQuestion(question) {
     // Selecteer het element dat we gaan aanmaken of bewerken
@@ -22,7 +26,7 @@ async function initialiseQuestion() {
     const difficulty = urlParams.get("difficulty") || "easy";
 
     // Afhankelijk van de moeilijkheidsgraad, haal de juiste vragen op
-    const questions = difficulty === "medium"
+    questions = difficulty === "medium"
         ? await getMediumQuestions()
         : await getEasyQuestions();
     
@@ -43,14 +47,35 @@ function renderAnswer(answers) {
     const $answerList = document.getElementById("answers-buttons");
     $answerList.innerHTML = "";
 
-    // Itereer over de antwoorden en plaats ze in de HTML
     for (const answer of answers) {
-        // += betekent dat we de bestaande inhoud behouden en er nieuwe inhoud aan toevoegen
-        $answerList.innerHTML += 
-        `
-            <li class="answers-button">
-            ${answer}
-            </li>
-        `
+        const li = document.createElement("li");
+        li.classList.add("answers-button");
+        li.textContent = answer;
+
+        // EVENT LISTENER
+        li.addEventListener("click", () => {
+            checkAnswer(answer);
+        });
+
+        $answerList.appendChild(li);
+    }
+}
+
+function checkAnswer(selectedAnswer) {
+    const currentQuestion = questions[currentQuestionIndex];
+
+    if (selectedAnswer === currentQuestion.answers[currentQuestion.correctAnswer]) {
+        alert("Je antwoord is juist!");
+        score++;
+    } else {
+        alert(`Het juiste antwoord was ${currentQuestion.answers[currentQuestion.correctAnswer]}`);
+    }
+    currentQuestionIndex++;
+
+        if (currentQuestionIndex < questions.length) {
+        renderQuestion(questions[currentQuestionIndex]);
+        renderAnswer(questions[currentQuestionIndex].answers);
+        } else {
+        alert(`Je hebt alle vragen beantwoord! Je score is: ${score}/${questions.length}`);
     }
 }
